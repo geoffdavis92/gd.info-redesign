07e08f9d9162efec208c5a361c412ec3a0fa6606
geoffdavis92 <geoffdavis92@gmail.com>
2014-08-04 16:16:41 -0500

    Add Feedback Fxn with JS
    Updated footer.js to include a link to pop-out feedback form!

--- js/footer.js
+++ js/footer.js
@@ -3,6 +3,8 @@
 var cFoot = document.createElement("footer");
 var cFL = document.createElement("div");
 var cFA = document.createElement("a");
+var spacer = document.createElement("a");
+var cFF = document.createElement("a");
 
 
 function createF(){
@@ -14,23 +16,40 @@ function createF(){
 
   document.body.cFA;
   cFoot.appendChild(cFA).setAttribute("href","../gd.info-redesign/");
-  var footLink = document.createTextNode("2014 Geoffrey Davis");
+  var footLink = document.createTextNode("2014 Geoffrey Davis   ");
   cFA.appendChild(footLink);
+
+  document.body.spacer;
+  var spacerNode = document.createTextNode("..........");
+  cFoot.appendChild(spacer);
+  spacer.appendChild(spacerNode);
+
+  document.body.cFF;
+  cFoot.appendChild(cFF).setAttribute("onclick", "feedback()");
+  var feedback = document.createTextNode("   Submit Feedback");
+  cFF.appendChild(feedback);
 
     function styleF(){
       cFoot.style.fontFamily = "Droid Sans, Helvetica, sans-serif";
       cFoot.style.width = "100%";
       cFoot.style.textAlign = "center";
-      cFoot.style.marginTop = "2.5%";
+      cFoot.style.marginTop = "1.75%";
       cFL.style.width = "80%";
       cFL.style.height = "2px";
       cFL.style.backgroundColor = "#ddd";
       //cFL.style.borderRadius = "10px";
       cFL.style.marginLeft = "10.1%";
       cFL.style.marginTop = ".75%";
-      cFL.style.marginBottom = ".25%";
+      cFL.style.marginBottom = ".75%";
+      spacer.style.color = "transparent";
     //  cFA.style.color = "#bbb";
 //      cFoot.style.marginLeft = "0%";
     }
     styleF();
 }
+
+function feedback(){
+  window.open("feedback.html", "_blank");
+}
+
+//("href","../gd.info-redesign/feedback.html");
